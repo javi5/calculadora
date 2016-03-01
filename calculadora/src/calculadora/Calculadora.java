5d508c93abf9b3c86e21dc59b66e8a671dd8cc90
Profesor <Profesor@Ternasco>
2016-03-01 16:31:07 +0100

    Mensaje creado

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -14,6 +14,11 @@ public class Calculadora {
     /**
      * @param args the command line arguments
      */
+    private static void mensaje(){
+        System.out.println ("Trabajamos aquí");
+    }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
